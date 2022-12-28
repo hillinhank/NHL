acae9882eecfb7a41ec082077307f7aa36da9dad
hillinhank <101481759+hillinhank@users.noreply.github.com>
2022-12-28 11:40:16 -0600

    Add files via upload

--- convert_data_attempt.py
+++ convert_data_attempt.py
@@ -1,9 +1,16 @@
 import pyreadr
-import pandas as pd
-result = pyreadr.read_w("/Users/user/data/NHL/Data/2021-11-11_rosters.rds") # also works for RData
-teams = pyreadr.read_r('/Users/user/data/NHL/Data/2021-11-11_teams.rds')
-players = teams = pyreadr.read_r('/Users/user/data/NHL/Data/2021-11-12_players.rds')
+import numpy as np
 
-# done! let's see what we got
-print(result.keys())
-df1 = result["df1"]
+rosters = pyreadr.read_r('2021-11-11_rosters.rds')
+df_rosters=rosters[None]
+df_rosters
+
+
+teams = pyreadr.read_r('2021-11-11_teams.rds')
+df_teams=teams[None]
+df_teams
+
+players = pyreadr.read_r('2021-11-12_players.rds')
+df_players=players[None]
+df_players
+
